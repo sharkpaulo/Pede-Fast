366bd9aa0631582db7441306bcb801bcbc98fcdb
sharkpaulo <53863276+sharkpaulo@users.noreply.github.com>
2021-03-07 11:49:14 -0300

    Delete Pede-fast directory

--- Pede-fast/includes/menu.inc.php
+++
@@ -1,9 +0,0 @@
-<nav class="blue-grey">
-    <div class="nav-waper container">
-        <div class="brand-logo light">PEDE FAST</div>
-        <ul class="right">
-            <li><a href="index.php"><i class="material-icons left">account_circle</i> Cadastro</a></li>
-            <li><a href="consultas.php"><i class="material-icons left">search</i> Consultas</a></li>
-        </ul>
-    </div><!--nav-wraper/container-->
-</nav>
