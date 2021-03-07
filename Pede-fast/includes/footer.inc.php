1c77478af6e5b5b9d52bde44fe18aebd88120a65
sharkpaulo <53863276+sharkpaulo@users.noreply.github.com>
2021-03-07 11:50:27 -0300

    Add files via upload

---
+++ Pede-fast/includes/footer.inc.php
@@ -0,0 +1,12 @@
+<!--ARQUIVOS JQUERY E JAVASCRIPT-->
+<script type="text/javascript" src="materialize/js/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+    <!--INICIALIZAÇÃO JQUERY-->
+<script type="text/javascript">
+    $(document).ready(funcion(){
+
+    });
+</script>
+</body>
+</html>
